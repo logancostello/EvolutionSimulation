8d17b4834fe5b459726bd107c3fbdf30f4335b13
logancostello <locostel@calpoly.edu>
2026-03-31 13:13:19 -0700

    fix: reproduction no longer adds to view while iterating

--- src/systems/reproduction_system.cpp
+++ src/systems/reproduction_system.cpp
@@ -11,11 +11,15 @@ ReproductionSystem::ReproductionSystem(entt::registry& registry, CreatureFactory
 void ReproductionSystem::update(float time) {
     auto view = registry.view<Creature, TimeOf>();
 
+    std::vector<entt::entity> to_reproduce;
     for (auto [entity, time_of] : view.each()) {
-
         if (time - time_of.last_reproduction > TIME_FOR_REPRODUCTION) {
             time_of.last_reproduction = time;
-            creature_factory.spawn_child(entity, time);
+            to_reproduce.push_back(entity);
         }
+    }
+
+    for (auto entity : to_reproduce) {
+        creature_factory.spawn_child(entity, time);
     }
 }
