b56f761f4c4fc26805e54a7e3e95477f26f0e16f
logancostello <locostel@calpoly.edu>
2026-03-31 09:05:40 -0700

    add: funcs for getting basic inputs and outputs

--- src/factories/brain_factory.cpp
+++ src/factories/brain_factory.cpp
@@ -3,20 +3,27 @@
 
 BrainFactory::BrainFactory(entt::registry& registry) : registry(registry) {};
 
-// Basic for now just to get outputs going
+std::vector<InputNode> BrainFactory::get_basic_inputs() {
+    return {
+        InputNode(0, 0, InputSource::Energy),
+        InputNode(1, 0, InputSource::DistToFood),
+        InputNode(2, 0, InputSource::DirToFood)
+    };
+};
+
+std::vector<OutputNode> BrainFactory::get_basic_outputs() {
+    return {
+        OutputNode(3, 0, OutputSource::VelocityMag),
+        OutputNode(4, 0, OutputSource::VelocityDir)
+    };
+};
+
 void BrainFactory::create_basic_brain(entt::entity& entity) {
 
-    InputNode input1 = InputNode(0, 0, InputSource::Energy);
-    InputNode input2 = InputNode(1, 0, InputSource::Energy);
-    InputNode input3 = InputNode(2, 0, InputSource::Energy);
-
-    OutputNode output1 = OutputNode(3, 0, OutputSource::VelocityMag);
-    OutputNode output2 = OutputNode(4, 0, OutputSource::VelocityDir);
-
-    std::vector<InputNode> input_nodes = {input1, input2, input3};
-    std::vector<OutputNode> output_nodes = {output1, output2};
+    std::vector<InputNode> input_nodes = get_basic_inputs();
+    std::vector<OutputNode> output_nodes = get_basic_outputs();
     std::vector<Node> hidden_nodes = {};
     std::vector<Edge> edges = {Edge(0, 3, 5), Edge(0, 4, 5), Edge(1, 3, 5), Edge(1, 4, 5), Edge(2, 3, 5), Edge(2, 4, 5)};
 
-    registry.emplace<Brain>(entity, input_nodes, output_nodes, hidden_nodes, edges);
+    registry.emplace<Brain>(entity, input_nodes.size(), output_nodes.size(), input_nodes, output_nodes, hidden_nodes, edges);
 };
--- src/factories/brain_factory.h
+++ src/factories/brain_factory.h
@@ -1,10 +1,14 @@
 #include <entt/entt.hpp>
+#include "components/brain/node.h"
 
 class BrainFactory {
     public:
         BrainFactory(entt::registry& registry);
         void create_basic_brain(entt::entity& entity);
 
+
     private:
         entt::registry& registry;
+        std::vector<InputNode> get_basic_inputs();
+        std::vector<OutputNode> get_basic_outputs();
 };
