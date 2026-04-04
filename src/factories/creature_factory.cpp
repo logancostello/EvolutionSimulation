668ebbb423e3b6c367a212271816d869db0f8068
logancostello <locostel@calpoly.edu>
2026-04-04 12:23:42 -0700

    fix: replace uses of <random> with random.h

--- src/factories/creature_factory.cpp
+++ src/factories/creature_factory.cpp
@@ -1,6 +1,6 @@
 #include "factories/creature_factory.h"
 #include "components/components.h"
-#include <random>
+#include "random.h"
 #include <cmath>
 
 CreatureFactory::CreatureFactory(entt::registry& registry) 
@@ -10,15 +10,13 @@ CreatureFactory::CreatureFactory(entt::registry& registry)
 
 void CreatureFactory::spawn_random(int world_size_x, int world_size_y, float time) {
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> pos_x_dist(world_size_x / -2.0f, world_size_x / 2.0f);
-    std::uniform_real_distribution<float> pos_y_dist(world_size_y / -2.0f, world_size_y / 2.0f);
-    std::uniform_real_distribution<float> vel_dist(-50.0f, 50.0f);
-
     entt::entity creature = registry.create();
-    registry.emplace<Position>(creature, pos_x_dist(gen), pos_y_dist(gen));
-    registry.emplace<Velocity>(creature, vel_dist(gen), vel_dist(gen));
+    registry.emplace<Position>(
+        creature, 
+        Random::float_range(world_size_x / -2.0f, world_size_x / 2.0f),
+        Random::float_range(world_size_y / -2.0f, world_size_y / 2.0f)
+    );
+    registry.emplace<Velocity>(creature, 0, 0);
     registry.emplace<Color>(creature, 255, 255, 255);
     registry.emplace<Size>(creature, 5.0f);
     registry.emplace<Energy>(creature, 10);
