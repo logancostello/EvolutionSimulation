8f535a1968c5c15dadb822e85f0b803b7cb6f992
logancostello <locostel@calpoly.edu>
2026-04-06 09:43:14 -0700

    add: change mutation rates to favor complexity and stability

--- src/mutators/brain_mutator.cpp
+++ src/mutators/brain_mutator.cpp
@@ -3,22 +3,22 @@
 #include "random.h"
 #include <algorithm>
 
-const float PROB_WEIGHT_FLIP = 0.05;
-const float PROB_WEIGHT_CHANGE = 0.3;
-const float PROB_BIAS_CHANGE = 0.3;
-const float PROB_TAU_CHANGE = 0.3;
-const float PROB_NEW_EDGE = 0.1;
-const float PROB_REMOVE_EDGE = 0.1;
-const float PROB_NEW_NODE = 0.05;
-const float PROB_REMOVE_NODE = 0.05;
-const float PROB_MUTATE_ACT = 0.1;
-
-const float WEIGHT_OFFSET_MEAN = 0;
-const float WEIGHT_OFFSET_STD = 0.5;
-const float BIAS_OFFSET_MEAN = 0;
-const float BIAS_OFFSET_STD = 0.5;
-const float TAU_OFFSET_MEAN = 0;
-const float TAU_OFFSET_STD = 0.15;
+const float PROB_WEIGHT_FLIP = 0.02;      
+const float PROB_WEIGHT_CHANGE = 0.4;     
+const float PROB_BIAS_CHANGE = 0.3;       
+const float PROB_TAU_CHANGE = 0.2;        
+const float PROB_NEW_EDGE = 0.2;          
+const float PROB_REMOVE_EDGE = 0.05;      
+const float PROB_NEW_NODE = 0.1;          
+const float PROB_REMOVE_NODE = 0.02;      
+const float PROB_MUTATE_ACT = 0.05;  
+     
+const float WEIGHT_OFFSET_MEAN = 0;       
+const float WEIGHT_OFFSET_STD = 0.3;      
+const float BIAS_OFFSET_MEAN = 0;         
+const float BIAS_OFFSET_STD = 0.3;        
+const float TAU_OFFSET_MEAN = 0;          
+const float TAU_OFFSET_STD = 0.1;         
 
 BrainMutator::BrainMutator(entt::registry& registry) : registry(registry) {};
 
